212e5e9b9c046571d066423c6a6f7afbfe6e2274
hehongzhen <360032350@qq.com>
2020-05-20 09:45:01 +0800

    model add member variable

--- puppetmodel/src/main/java/com/mango/puppetmodel/Event.java
+++ puppetmodel/src/main/java/com/mango/puppetmodel/Event.java
@@ -1,5 +1,7 @@
 package com.mango.puppetmodel;
 
+import java.util.Map;
+
 /**
  * Event
  * 事件模型
@@ -9,4 +11,16 @@ package com.mango.puppetmodel;
  */
 @SuppressWarnings("unused")
 public class Event {
+
+    // 事件名称
+    public String event_name;
+
+    // 监听哪个应用，系统应用不需此参数
+    public String package_name;
+
+    // 1:注册 0:注销
+    public Map event_data;
+
+    // 事件上报结果(仅本地控制器使用) 0:新事件未上报 1:上报已成功
+    public int event_status;
 }
--- puppetmodel/src/main/java/com/mango/puppetmodel/EventWatcher.java
+++ puppetmodel/src/main/java/com/mango/puppetmodel/EventWatcher.java
@@ -9,4 +9,13 @@ package com.mango.puppetmodel;
  */
 @SuppressWarnings("unused")
 public class EventWatcher {
+
+    // 事件名称
+    public String event_name;
+
+    // 监听哪个应用，系统应用不需此参数
+    public String package_name;
+
+    // 1:注册 0:注销
+    public int watcher_status;
 }
