32afcbfd422b8ab406bb49787529522bb4f649ed
hehongzhen <360032350@qq.com>
2020-05-20 15:58:48 +0800

    systemplugin

---
+++ app/src/main/java/com/mango/puppetsystem/AppApplication.java
@@ -0,0 +1,28 @@
+package com.mango.puppetsystem;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.mango.puppet.systemplugin.SystemPluginManager;
+import com.mango.puppet.systemplugin.i.ISystemPluginListener;
+
+public class AppApplication extends Application implements ISystemPluginListener {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SystemPluginManager.getInstance().setSystemPluginListener(this, this);
+    }
+
+    @Override
+    public void onBatteryChange(int intLevel, int intScale) {
+        Log.d("AppApplication", "onBatteryChange:" + intLevel + "/" + intScale);
+
+    }
+
+    @Override
+    public void onScreenChange(boolean isOff) {
+        Log.d("AppApplication", "onScreenChange:" + isOff);
+    }
+}
