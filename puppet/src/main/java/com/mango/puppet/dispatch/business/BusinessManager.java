fea8861a4e2949d4b80f6911f4be88e2c04bcbbb
elapidae13 <huyuwei@icaipiao123.com>
2020-05-22 15:06:44 +0800

    business

--- puppet/src/main/java/com/mango/puppet/dispatch/business/BusinessManager.java
+++ puppet/src/main/java/com/mango/puppet/dispatch/business/BusinessManager.java
@@ -1,6 +1,11 @@
 package com.mango.puppet.dispatch.business;
 
 import com.mango.puppet.dispatch.business.i.IBusiness;
+import com.mango.puppet.log.LogManager;
+import com.mango.puppet.network.NetworkManager;
+import com.mango.puppet.network.i.INetwork;
+import com.mango.puppetmodel.UploadResourceModel;
+import java.util.List;
 
 /**
  * BusinessManager
@@ -21,7 +26,30 @@ public class BusinessManager implements IBusiness {
 
     /************   IBusiness   ************/
     @Override
-    public void getUploadResourceWay(IUploadResourceWayResult result) {
+    public void getUploadResourceWay(final IUploadResourceWayResult mResult) {
+        NetworkManager.getInstance().requestUploadResourceWay(new INetwork.IRequestResult() {
+            @Override
+            public void onSuccess(Object result) {
+                if (result instanceof List){
+                    if (((List) result).size()>0&&((List) result).get(0) instanceof UploadResourceModel){
+                        mResult.onSuccess((List)result);
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int errorCode, String errorMessage) {
+                if (mResult != null){
+                    mResult.onError();
+                    LogManager.getInstance().recordLog("请求资源时路径时，网络连接失败");
+                }
+            }
+
+            @Override
+            public void onNetworkError() {
+                LogManager.getInstance().recordLog("请求资源时路径时，网络连接失败");
+            }
+        });
 
     }
 }
